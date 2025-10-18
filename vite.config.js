b5497428c5532d617eadfc0874d2db62708b0c28
Denirant <dbrylev18@gmail.com>
2025-10-18 12:10:02 +0300

    fixes

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 export default defineConfig({
   plugins: [svelte()],
   server: {
-    port: 9850
+    port: 9850,
+    allowedHosts: ['www.axonai.tech']
   }
 })
